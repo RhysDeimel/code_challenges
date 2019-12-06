016d3b1f910ef64e1ea0b45292167740925f63eb
RhysDeimel <rdeimel@assemblypayments.com>
2019-12-06 16:31:49 +1100

    working on the second solution. Renaming some things

--- advent_of_code/2019/01/solution.py
+++ advent_of_code/2019/01/solution.py
@@ -3,18 +3,21 @@ def get_inputs(file):
         lines = fd.read().splitlines()
         return [int(line) for line in lines]
 
-def calc_module_fuel(mass):
+
+def simple_calc_module_fuel(mass):
     return (mass // 3) - 2
 
 
-def main(modules):
+def soultion_part_one(modules):
     total_fuel = 0
     for module in modules:
-        total_fuel += calc_module_fuel(module)
+        total_fuel += simple_calc_module_fuel(module)
 
-    print(total_fuel)
+    print(f"Part one solution: {total_fuel}")
     return total_fuel
 
 
+# def calc
+
 if __name__ == "__main__":
-    main(get_inputs("input.txt"))
+    soultion_part_one(get_inputs("input.txt"))
--- advent_of_code/2019/01/tests.py
+++ advent_of_code/2019/01/tests.py
@@ -8,18 +8,18 @@ class TestUnit:
         assert result[-1] == 72622
 
     def test_calc_module_fuel(self):
-        assert solution.calc_module_fuel(12) == 2
-        assert solution.calc_module_fuel(14) == 2
-        assert solution.calc_module_fuel(1969) == 654
-        assert solution.calc_module_fuel(100756) == 33583
+        assert solution.simple_calc_module_fuel(12) == 2
+        assert solution.simple_calc_module_fuel(14) == 2
+        assert solution.simple_calc_module_fuel(1969) == 654
+        assert solution.simple_calc_module_fuel(100756) == 33583
 
 
 class TestFunctional:
-    def test_main_with_single_item(self):
-        assert solution.main([12]) == 2
-        assert solution.main([14]) == 2
-        assert solution.main([1969]) == 654
-        assert solution.main([100756]) == 33583
+    def test_soultion_part_one_with_single_item(self):
+        assert solution.soultion_part_one([12]) == 2
+        assert solution.soultion_part_one([14]) == 2
+        assert solution.soultion_part_one([1969]) == 654
+        assert solution.soultion_part_one([100756]) == 33583
 
-    def test_main_with_mutliple_items(self):
-        assert solution.main([12, 14, 1969, 100756]) == (2 + 2 + 654 + 33583)
+    def test_soultion_part_one_with_mutliple_items(self):
+        assert solution.soultion_part_one([12, 14, 1969, 100756]) == (2 + 2 + 654 + 33583)
