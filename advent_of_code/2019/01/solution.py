d7880c77c1fbafba057f70d73a9c03d06eabaf1b
RhysDeimel <rdeimel@assemblypayments.com>
2019-12-09 15:19:34 +1100

    second part complete and additional refactoring

--- advent_of_code/2019/01/solution.py
+++ advent_of_code/2019/01/solution.py
@@ -8,16 +8,25 @@ def simple_calc_module_fuel(mass):
     return (mass // 3) - 2
 
 
-def soultion_part_one(modules):
+def calc_module_fuel(mass):
+    fuel = (mass // 3) - 2
+
+    if fuel <= 0:
+        return 0
+    else:
+        return calc_module_fuel(fuel) + fuel
+
+
+def solution(modules, f):
     total_fuel = 0
     for module in modules:
-        total_fuel += simple_calc_module_fuel(module)
+        total_fuel += f(module)
 
-    print(f"Part one solution: {total_fuel}")
     return total_fuel
 
 
-# def calc
-
 if __name__ == "__main__":
-    soultion_part_one(get_inputs("input.txt"))
+    inputs = get_inputs("input.txt")
+
+    print(f"Part one solution: {solution(inputs, simple_calc_module_fuel)}")
+    print(f"Part two solution: {solution(inputs, calc_module_fuel)}")
--- advent_of_code/2019/01/tests.py
+++ advent_of_code/2019/01/tests.py
@@ -7,19 +7,40 @@ class TestUnit:
         assert result[0] == 60077
         assert result[-1] == 72622
 
-    def test_calc_module_fuel(self):
+    def test_simple_calc_module_fuel(self):
         assert solution.simple_calc_module_fuel(12) == 2
         assert solution.simple_calc_module_fuel(14) == 2
         assert solution.simple_calc_module_fuel(1969) == 654
         assert solution.simple_calc_module_fuel(100756) == 33583
 
+    def test_calc_module_fuel(self):
+        assert solution.calc_module_fuel(14) == 2
+        assert solution.calc_module_fuel(1969) == 966
+        assert solution.calc_module_fuel(100756) == 50346
+
 
 class TestFunctional:
-    def test_soultion_part_one_with_single_item(self):
-        assert solution.soultion_part_one([12]) == 2
-        assert solution.soultion_part_one([14]) == 2
-        assert solution.soultion_part_one([1969]) == 654
-        assert solution.soultion_part_one([100756]) == 33583
+    def test_simple_solution_with_single_item(self):
+        func = solution.simple_calc_module_fuel
 
-    def test_soultion_part_one_with_mutliple_items(self):
-        assert solution.soultion_part_one([12, 14, 1969, 100756]) == (2 + 2 + 654 + 33583)
+        assert solution.solution([12], func) == 2
+        assert solution.solution([14], func) == 2
+        assert solution.solution([1969], func) == 654
+        assert solution.solution([100756], func) == 33583
+
+    def test_simple_solution_with_multiple_items(self):
+        func = solution.simple_calc_module_fuel
+
+        assert solution.solution([12, 14, 1969, 100756], func) == (2 + 2 + 654 + 33583)
+
+    def test_solution_part_with_single_item(self):
+        func = solution.calc_module_fuel
+
+        assert solution.solution([14], func) == 2
+        assert solution.solution([1969], func) == 966
+        assert solution.solution([100756], func) == 50346
+
+    def test_solution_part_two_with_multiple_items(self):
+        func = solution.calc_module_fuel
+
+        assert solution.solution([14, 1969, 100756], func) == (2 + 966 + 50346)
