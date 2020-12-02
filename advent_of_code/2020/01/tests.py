ac841e7346203da928a2c39fe743eded1bb6f0ee
RhysDeimel <deimelr@hotmail.com>
2020-12-02 23:53:39 +1100

    New year, new advent. Let's try finish this time

---
+++ advent_of_code/2020/01/tests.py
@@ -0,0 +1,15 @@
+import solution
+
+class TestUnit:
+    def test_get_inputs(self):
+        result = solution.get_inputs("input.txt")
+        assert result[0] == 1
+        assert result[-1] == 0
+
+# class TestFunctional:
+#     def test_program_one(self):
+#         given = [1, 1, 1, 4, 99, 5, 6, 0, 99]
+#         expected = [30, 1, 1, 4, 2, 5, 6, 0, 99]
+
+#         solution.program_one(given)
+#         assert given == expected
