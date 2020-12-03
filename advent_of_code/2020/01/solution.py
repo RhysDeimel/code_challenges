21efd075738c56b573b45b2885473755e6edbb38
RhysDeimel <deimelr@hotmail.com>
2020-12-03 21:09:53 +1100

    01: solution one & two complete

--- advent_of_code/2020/01/solution.py
+++ advent_of_code/2020/01/solution.py
@@ -1,9 +1,27 @@
+import itertools
+import math
+
 def get_inputs(file):
     with open(file, "r") as fd:
         lines = fd.readlines()
         return [int(num) for num in lines]
 
-# >>> from itertools import combinations
-# >>> L = [1, 2, 3, 4]
-# >>> [",".join(map(str, comb)) for comb in combinations(L, 3)]
-# ['1,2,3', '1,2,4', '1,3,4', '2,3,4']
+def find_nums_that_sum_to(items, n_entries, sum_to):
+    combinations = [subsequence for subsequence in itertools.combinations(items, n_entries)]
+    sums = [sum(element) for element in combinations]
+    return combinations[sums.index(sum_to)]
+
+def program_one(inputs):
+    nums = find_nums_that_sum_to(inputs, 2, 2020)
+    return math.prod(nums)
+
+def program_two(inputs):
+    nums = find_nums_that_sum_to(inputs, 3, 2020)
+    return math.prod(nums)
+    pass
+
+
+if __name__ == "__main__":
+    inputs = get_inputs("input.txt")
+    print(f"Part one solution: {program_one(inputs)}")
+    print(f"Part two solution: {program_two(inputs)}")
--- advent_of_code/2020/01/tests.py
+++ advent_of_code/2020/01/tests.py
@@ -1,15 +1,33 @@
-import solution
+import solution as s
 
 class TestUnit:
     def test_get_inputs(self):
-        result = solution.get_inputs("input.txt")
+        result = s.get_inputs("input.txt")
         assert result[0] == 1975
         assert result[-1] == 1322
 
-# class TestFunctional:
-#     def test_program_one(self):
-#         given = [1, 1, 1, 4, 99, 5, 6, 0, 99]
-#         expected = [30, 1, 1, 4, 2, 5, 6, 0, 99]
+    def test_find_nums_that_sum_to_can_handle_two_digits(self):
+        given = [1721, 979, 366, 299, 675, 1456]
+        expected = {1721, 299} # 2020
+        result = s.find_nums_that_sum_to(given, 2, sum(expected)) 
+        assert set(result) == expected
 
-#         solution.program_one(given)
-#         assert given == expected
+    def test_find_nums_that_sum_to_can_handle_three_digits(self):
+        given = [1721, 979, 366, 299, 675, 1456]
+        expected = {979, 366, 675} # 2020
+        result = s.find_nums_that_sum_to(given, 3, sum(expected)) # 2020
+        assert set(result) == expected
+
+
+class TestFunctional:
+    def test_program_one(self):
+        given = [1721, 979, 366, 299, 675, 1456]
+        expected = 514579
+
+        assert s.program_one(given) == expected
+
+    def test_program_two(self):
+        given = [1721, 979, 366, 299, 675, 1456]
+        expected = 241861950
+
+        assert s.program_two(given) == expected
