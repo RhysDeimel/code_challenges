815d4df21f00c7d2b6d0b988b753b8cb0343919a
RhysDeimel <deimelr@hotmail.com>
2020-12-08 00:05:12 +1100

    03: part two complete, and refactor of part one

--- advent_of_code/2020/03/solution.py
+++ advent_of_code/2020/03/solution.py
@@ -3,24 +3,42 @@ def get_inputs(file):
         return fd.read().splitlines()
 
 
-def program_one(grid):
-    # right 3, down one
-    width = len(grid[0])
+def find_trees(vector, grid):
+    max_width = len(grid[0])
+
     num_trees = 0
+
     x_pos = 0
-    for row in grid:
-        if row[x_pos % width] == "#":
-            num_trees += 1
-        x_pos += 3
+    y_pos = 0
 
-    return num_trees
+    while True:
+        x_pos += vector[0]
+        y_pos += vector[1]
+
+        try:
+            if grid[y_pos][x_pos % max_width] == "#":
+                num_trees += 1
+
+        except IndexError:
+            return num_trees
 
 
-def program_two(inputs):
-    pass
+def program_one(grid):
+    return find_trees((3, 1), grid)
+
+
+def program_two(grid):
+    def tree_product(vectors, grid):
+        prod = 1
+        for vec in vectors:
+            prod *= find_trees(vec, grid)
+        return prod
+
+    vectors = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    return tree_product(vectors, grid)
 
 
 if __name__ == "__main__":
     inputs = get_inputs("input.txt")
     print(f"Part one solution: {program_one(inputs)}")
-    # print(f"Part two solution: {program_two(inputs)}")
+    print(f"Part two solution: {program_two(inputs)}")
--- advent_of_code/2020/03/test.py
+++ advent_of_code/2020/03/test.py
@@ -21,16 +21,20 @@ class TestUnit:
         assert result[0] == ".....#.........#...#..##....#.."
         assert result[-1] == "..#..#........#...#.......#...."
 
+    def test_find_trees_1_1_gives_2(self):
+        assert s.find_trees((1, 1), test_input) == 2
 
-#     def test_process_inputs_creates_named_tuples(self):
-#         given = ["1-3 a: abcde", "1-3 b: cdefg"]
-#         result = s.process_inputs(given)
+    def test_find_trees_3_1_gives_7(self):
+        assert s.find_trees((3, 1), test_input) == 7
 
-#         assert result[0].min == 1
-#         assert result[0].max == 3
-#         assert result[0].letter == "a"
-#         assert result[0].password == "abcde"
-#         assert result[1].password == "cdefg"
+    def test_find_trees_5_1_gives_3(self):
+        assert s.find_trees((5, 1), test_input) == 3
+
+    def test_find_trees_7_1_gives_4(self):
+        assert s.find_trees((7, 1), test_input) == 4
+
+    def test_find_trees_1_2_gives_2(self):
+        assert s.find_trees((1, 2), test_input) == 2
 
 
 class TestFunctional:
@@ -38,8 +42,6 @@ class TestFunctional:
 
         assert s.program_one(test_input) == 7
 
+    def test_program_two(self):
 
-#     def test_program_two(self):
-#         given = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
-
-#         assert s.program_two(given) == 1
+        assert s.program_two(test_input) == 336
