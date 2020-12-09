838e8249d65b269fb4e254a69ebb450ddc29151b
RhysDeimel <deimelr@hotmail.com>
2020-12-09 23:59:46 +1100

    04: working towards a part two solution

--- advent_of_code/2020/04/solution.py
+++ advent_of_code/2020/04/solution.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
-class Passport:
+class Pssport:
     byr: int
     iyr: int
     eyr: int
@@ -13,29 +13,93 @@ class Passport:
     cid: int = None
 
 
+class Passport:
+    def __init__(self, byr, iyr, eyr, hgt, hcl, ecl, pid, cid):
+        self.byr = byr
+        self.iyr = iyr
+        self.eyr = eyr
+        self.hgt = hgt
+        self.hcl = hcl
+        self.ecl = ecl
+        self.pid = pid
+        self.cid = cid
+
+    @property
+    def byr(self):
+        return self._byr
+
+    @byr.setter
+    def byr(self, value):
+        if value < "1920" or value > "2002":
+            raise ValueError("Byr is not valid")
+        self._byr = value
+
+    @property
+    def iyr(self):
+        return self._iyr
+
+    @iyr.setter
+    def iyr(self, value):
+        if value < "2010" or value > "2020":
+            raise ValueError("Iyr is not valid")
+        self._iyr = value
+
+#     @eyr.setter
+#     def eyr(self, value):
+#         pass
+
+#     @hgt.setter
+#     def hgt(self, value):
+#         pass
+
+#     @hcl.setter
+#     def hcl(self, value):
+#         pass
+
+#     @ecl.setter
+#     def ecl(self, value):
+#         pass
+
+#     @pid.setter
+#     def pid(self, value):
+#         pass
+
+#     @cid.setter
+#     def cid(self, value):
+#         pass
+
+
 def get_inputs(file):
     with open(file, "r") as fd:
         passports = fd.read().rstrip().split("\n\n")
         return [passport.replace("\n", " ") for passport in passports]
 
 
-def program_one(inputs):
-    cleaned = [
+def format_inputs(inputs):
+    split = [
         [passport_data.split(":") for passport_data in item.split()] for item in inputs
     ]
-    passports = [dict(item) for item in cleaned]
+    return [dict(item) for item in split]
+
+
+def program_one(inputs):
+    passports = format_inputs(inputs)
 
     valid_passports = []
     for item in passports:
         try:
-            valid_passports.append(Passport(**item))
+            valid_passports.append(Pssport(**item))
         except TypeError:
             pass
 
     return len(valid_passports)
 
 
+def program_two(inputs):
+    pass
+
+
 if __name__ == "__main__":
     inputs = get_inputs("input.txt")
     print(f"Part one solution: {program_one(inputs)}")
-    # print(f"Part two solution: {program_two(inputs)}")
+    print(f"Part two solution: {program_two(inputs)}")
--- advent_of_code/2020/04/tests.py
+++ advent_of_code/2020/04/tests.py
@@ -1,10 +1,36 @@
+import copy
+import pytest
 import solution as s
 
-test_input = [
+p1_test_input = [
     "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm",
     "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884 hcl:#cfa07d byr:1929",
     "hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm",
     "hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in",
+]
+
+p2_test_input = [
+    "eyr:1972 cid:100 hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926",
+    "iyr:2019 hcl:#602927 eyr:1967 hgt:170cm ecl:grn pid:012533040 byr:1946",
+    "hcl:dab227 iyr:2012 ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277",
+    "hgt:59cm ecl:zzz eyr:2038 hcl:74454a iyr:2023 pid:3556412378 byr:2007",
+    "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980 hcl:#623a2f",
+    "eyr:2029 ecl:blu cid:129 byr:1989 iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm",
+    "hcl:#888785 hgt:164cm byr:2001 iyr:2015 cid:88 pid:545766238 ecl:hzl eyr:2022",
+    "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719",
+]
+
+formatted_input = [
+    {
+        "eyr": "1972",
+        "cid": "100",
+        "hcl": "#18171d",
+        "ecl": "amb",
+        "hgt": "170",
+        "pid": "186cm",
+        "byr": "1926",
+        "iyr": "2018",
+    }
 ]
 
 
@@ -17,7 +43,50 @@ class TestUnit:
         )
         assert result[-1] == "byr:2000 ecl:hzl eyr:2029 iyr:2011 hcl:#866857 hgt:74in"
 
+    def test_format_inputs(self):
+        given = p2_test_input[0:1] # slice because I want a list
+        expected = copy.deepcopy(formatted_input)
+        assert s.format_inputs(given) == expected
+
+    def test_Passport_checks_byr(self):
+        valid_byr = ("1920","2002")
+        invalid_byr = ("1919","2003")
+
+        given = copy.deepcopy(formatted_input[0])
+        given["byr"] = valid_byr[0]
+
+        p = s.Passport(**given)
+        assert p.byr in valid_byr
+        p.byr = valid_byr[1]
+        assert p.byr in valid_byr
+
+        with pytest.raises(ValueError):
+            p.byr = invalid_byr[0]
+        with pytest.raises(ValueError):
+            p.byr = invalid_byr[1]
+
+
+    def test_Passport_checks_iyr(self):
+        valid_iyr = ("2010","2020")
+        invalid_iyr = ("2009","2021")
+
+        given = copy.deepcopy(formatted_input[0])
+        given["iyr"] = valid_iyr[0]
+
+        p = s.Passport(**given)
+        assert p.iyr in valid_iyr
+        p.iyr = valid_iyr[1]
+        assert p.iyr in valid_iyr
+
+        with pytest.raises(ValueError):
+            p.iyr = invalid_iyr[0]
+        with pytest.raises(ValueError):
+            p.iyr = invalid_iyr[1]
+
 
 class TestFunctional:
     def test_program_one(self):
-        assert s.program_one(test_input) == 2
+        assert s.program_one(p1_test_input) == 2
+
+    def test_program_two(self):
+        assert s.program_two(p2_test_input) == 4
