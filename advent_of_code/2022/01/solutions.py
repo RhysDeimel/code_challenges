39b9cf0c8d11f21cd7018e931d7d2b1747455125
RhysDeimel <deimelr@hotmail.com>
2022-12-01 16:59:06 +1100

    2022 day 1 part 1

---
+++ advent_of_code/2022/01/solutions.py
@@ -0,0 +1,25 @@
+def parse(puzzle_input):
+    return ([int(item) for item in line.split("\n")] for line in puzzle_input.rstrip().split("\n\n"))
+
+
+def part1(data):
+    return max([sum(chunk) for chunk in data])
+
+
+def part2(data):
+    pass
+
+
+def solve(puzzle_input):
+    data = parse(puzzle_input)
+    solution1 = part1(data)
+    solution2 = part2(data)
+
+    print(f"Solution 1: {solution1}")
+
+
+if __name__ == "__main__":
+    with open("input.txt") as fd:
+        puzzle_input = fd.read()
+
+    solve(puzzle_input)
---
+++ advent_of_code/2022/01/test_aoc.py
@@ -0,0 +1,27 @@
+import solutions
+import pytest
+
+
+@pytest.fixture
+def example1():
+    with open("example1.txt") as fd:
+        puzzle_input = fd.read()
+
+    return solutions.parse(puzzle_input)
+
+
+def test_parse_example1(example1):
+    assert list(example1) == [
+        [1000, 2000, 3000],
+        [4000],
+        [5000, 6000],
+        [7000, 8000, 9000],
+        [10000],
+    ]
+
+
+def test_part1_example1(example1):
+    assert solutions.part1(example1) == 24000
+
+def test_part2_example1(example1):
+    assert False
