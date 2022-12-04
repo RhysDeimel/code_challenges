1ff8a96137f832562aa01eeb692110c3f1937147
RhysDeimel <deimelr@hotmail.com>
2022-12-04 14:42:37 +1100

    2022 day 3 both parts

---
+++ advent_of_code/2022/03/solutions.py
@@ -0,0 +1,55 @@
+def parse(puzzle_input):
+    return [line for line in puzzle_input.rstrip().split("\n")]
+
+
+def part1(data):
+    result = 0
+
+    for line in data:
+        half = len(line) // 2
+        a = set(line[:half])
+        b = set(line[half:])
+
+        common = list(a & b)[0]
+
+        if common.isupper():
+            result += ord(common) - 38
+        else:
+            result += ord(common) - 96
+
+    return result
+
+
+def part2(data):
+    def chunks(lst, n):
+        """Yield successive n-sized chunks from lst."""
+        for i in range(0, len(lst), n):
+            yield lst[i : i + n]
+
+    result = 0
+
+    for a, b, c in chunks(data, 3):
+        common = list(set(a) & set(b) & set(c))[0]
+
+        if common.isupper():
+            result += ord(common) - 38
+        else:
+            result += ord(common) - 96
+
+    return result
+
+
+def solve(puzzle_input):
+    data = parse(puzzle_input)
+    solution1 = part1(data)
+    solution2 = part2(data)
+
+    print(f"Solution 1: {solution1}")
+    print(f"Solution 2: {solution2}")
+
+
+if __name__ == "__main__":
+    with open("input.txt") as fd:
+        puzzle_input = fd.read()
+
+    solve(puzzle_input)
