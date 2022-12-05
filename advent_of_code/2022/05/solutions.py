c7fcd13c6ef88f642caaa52e1aced32a5ca94364
RhysDeimel <deimelr@hotmail.com>
2022-12-05 19:25:40 +1100

    2022 day 5 part 1

---
+++ advent_of_code/2022/05/solutions.py
@@ -0,0 +1,91 @@
+def parse(puzzle_input):
+    raw_crates, raw_moves = puzzle_input.rstrip().split("\n\n")
+    crates = []
+    moves = []
+
+    # get the number of the last column
+    num_columns = int(raw_crates[-1])
+    raw_crates = raw_crates.split("\n")
+    line_length = len(raw_crates[-1]) + 1
+    # remove numbered column
+    raw_crates = raw_crates[:-1]
+    for row in raw_crates:
+        if (cur_len := len(row)) < line_length:
+            # equalise lines
+            row += " " * (line_length - cur_len)
+        # ignore the brackets
+        crates.append([letter for letter in row[1::4]])
+
+    raw_moves = [line for line in raw_moves.split("\n")]
+    for line in raw_moves:
+        items = line.split(" ")
+        nums_only = list(map(int, [items[1], items[3], items[5]]))
+        moves.append(nums_only)
+
+    return crates, moves
+
+
+class Ship:
+    def __init__(self, crates: list):
+        self.crates = crates
+
+    def pick(self, column: int):
+        for row in self.crates:
+            if (held := row[column - 1]) != " ":
+                self.held = held
+                row[column - 1] = " "
+                return
+
+    def place(self, column: int):
+        for row in self.crates[::-1]:
+            if row[column - 1] == " ":
+                row[column - 1] = self.held
+                self.held = None
+                return
+
+        new_row = [" " for pos in self.crates[0]]
+        self.crates.insert(0, new_row)
+        self.place(column)
+
+    def top_crates(self):
+        data = ""
+        for i, column in enumerate(self.crates[0]):
+            for row in self.crates:
+                if (letter := row[i]) != " ":
+                    data += letter
+                    break
+
+        return data
+
+
+def part1(data):
+    crates, moves = data
+
+    ship = Ship(crates)
+
+    for num, from_pos, to_pos in moves:
+        for i in range(num):  # move this many crates
+            ship.pick(from_pos)
+            ship.place(to_pos)
+
+    return ship.top_crates()
+
+
+def part2(data):
+    return None
+
+
+def solve(puzzle_input):
+    data = parse(puzzle_input)
+    solution1 = part1(data)
+    solution2 = part2(data)
+
+    print(f"Solution 1: {solution1}")
+    print(f"Solution 2: {solution2}")
+
+
+if __name__ == "__main__":
+    with open("input.txt") as fd:
+        puzzle_input = fd.read()
+
+    solve(puzzle_input)
---
+++ advent_of_code/2022/05/test_aoc.py
@@ -0,0 +1,48 @@
+import solutions
+import pytest
+
+
+@pytest.fixture
+def example1():
+    with open("example1.txt") as fd:
+        puzzle_input = fd.read()
+
+    return solutions.parse(puzzle_input)
+
+
+@pytest.fixture
+def input():
+    with open("input.txt") as fd:
+        puzzle_input = fd.read()
+
+    return solutions.parse(puzzle_input)
+
+
+def test_parse_example1(example1):
+    crates, moves = list(example1)
+    assert crates == [[" ", "D", " "], ["N", "C", " "], ["Z", "M", "P"]]
+    assert moves == [[1, 2, 1], [3, 1, 3], [2, 2, 1], [1, 1, 2]]
+
+
+def test_parse_input(input):
+    # explicit test for input because crate space is larger
+    crates, _ = list(input)
+    assert crates == [
+        ["N", " ", " ", "C", " ", "Z", " ", " ", " "],
+        ["Q", "G", " ", "V", " ", "S", " ", " ", "V"],
+        ["L", "C", " ", "M", " ", "T", " ", "W", "L"],
+        ["S", "H", " ", "L", " ", "C", "D", "H", "S"],
+        ["C", "V", "F", "D", " ", "D", "B", "Q", "F"],
+        ["Z", "T", "Z", "T", "C", "J", "G", "S", "Q"],
+        ["P", "P", "C", "W", "W", "F", "W", "J", "C"],
+        ["T", "L", "D", "G", "P", "P", "V", "N", "R"],
+    ]
+
+
+def test_part1_example1(example1):
+    assert solutions.part1(example1) == "CMZ"
+
+
+@pytest.mark.skip
+def test_part2_example1(example1):
+    assert solutions.part2(example1) == None
